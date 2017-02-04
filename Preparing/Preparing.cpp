e94cfa17a9a7cdfce38978b205aa74cb7b96d834
Max <Max-der-Monarch@yandex.ua>
2017-02-04 15:57:08 +0200

    Main file commited

---
+++ Preparing/Preparing.cpp
@@ -0,0 +1,19 @@
+// Preparing.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	int i=0;
+	for (i=0;i<6; ++i) {
+		cout << i;
+	}
+
+	_getch();
+    return 0;
+}
+
